89159cabe39c4ec0714f44b9cbc72c543415815d
Mooli Tayer <mtayer@redhat.com>
2017-07-12 13:01:39 +0300

    Change parameters

--- deployer-test.sh
+++ deployer-test.sh
@@ -6,6 +6,7 @@
 #
 
 BUILD_ID=8
+OPENSHIFT_ANSIBLE_REPO_URL=https://github.com/openshift/openshift-ansible
 OPENSHIFT_ANSIBLE_REF=openshift-ansible-3.6.140-1
 MASTER_IP=10.35.48.200
 INFRA_IPS="10.35.48.201 10.35.48.202"
@@ -14,6 +15,8 @@ WORKSPACE=$(realpath ../test_workspace)
 ADDITIONAL_REPOS="[{'id': 'example', 'name': 'EXAMPLE', 'baseurl': 'http://example.com/$basearch/os', 'enabled': 1, 'gpgcheck': 0}, {'id': 'fast-datapath', 'name': 'Fast Datapath', 'baseurl': 'http://pulp.dist.prod.ext.phx2.redhat.com/content/dist/rhel/server/7/7Server/$basearch/fast-datapath/os', 'enabled': 1, 'gpgcheck': 0}]"
 ROOT_PASSWORD="qum5net"
 DEPLOYERS_VERSION=v3.6
+
+# Imitate jenkins, works only with THIS repository and master
 rm -rf ${WORKSPACE}/*
 mkdir ${WORKSPACE}
 cp -r ../ocp-ansible-jenkins/* ${WORKSPACE}/
