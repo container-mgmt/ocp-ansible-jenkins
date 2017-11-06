a2f23f1b87d6a07045f69a14bc3903e5c866625c
Barak Azulay <bazulay@redhat.com>
2017-11-06 09:32:58 +0200

    Added creation of the PV exports to deployer.sh + creation of the pv yaml files
    Signed-off-by: Barak Azulay <bazulay@redhat.com>

--- deployer.sh
+++ deployer.sh
@@ -1,14 +1,33 @@
 BUILD_DIR="${WORKSPACE}/${BUILD_ID}"
-INVENTORY_TEMPLATE_PATH="${WORKSPACE}/inventory-template.ini"
+INVENTORY_TEMPLATE_PATH="${WORKSPACE}/inventory-template_ext_nfs.ini"
+PV_FILE_TEMPLATE="${WORKSPACE}/pv-template.yaml"
 INVENTORY_PATH="${BUILD_DIR}/inventory.ini"
 OPENSHIFT_ANSIBLE_PATH="${BUILD_DIR}/openshift-ansible"
 ENVIRONMENT_FILE="${BUILD_DIR}/environment"
 ID_FILE="${WORKSPACE}/../id_rsa"
 REDHAT_IT_ROOT_CA_PATH="/etc/pki/ca-trust/source/anchors/RH-IT-Root-CA.crt"
 NAME_PREFIX=${NAME_PREFIX:ocp}
+EXT_NFS_BASE_EXPORT_PATH_NORMALIZED=$(echo ${EXT_NFS_BASE_EXPORT_PATH} | sed 's./.\\/.g')
+CLUSTER_EXT_NFS_BASE_EXPORT_PATH="${EXT_NFS_BASE_EXPORT_PATH_NORMALIZED}\/${NAME_PREFIX}"
+TMP_MNT_PATH="${BUILD_DIR}/mnt"
+TMP_RESOURCE_DIR="${BUILD_DIR}/${NAME_PREFIX}_PVs"
+PREDEFINED_PVS_TO_CREATE="registry metrics logging logging-ops prometheus prometheus-alertmanager prometheus-alertbuffer"
+
 
 ansible --version
 export| tee ${ENVIRONMENT_FILE}
+
+
+sudo_mkdir_if_not_exist () {
+	DIRECTORY=${1}
+	if [ ! -d "${DIRECTORY}" ]; then
+		sudo mkdir "${DIRECTORY}"
+	else
+		echo "directory '${DIRECTORY}' already exist , skipping ..."
+	fi
+}
+
+
 
 mkdir ${BUILD_DIR}
 echo "#######################################################################"
@@ -17,6 +36,33 @@ echo "# Revision:   $(git log --oneline -1)"
 echo "# Rerun with: "
 echo ". ${ENVIRONMENT_FILE} ; $(realpath deployer)"
 echo "#######################################################################"
+
+#Creting the different nfs exports on the external NFS server by mounting it locally 
+mkdir ${TMP_MNT_PATH}
+sudo mount ${EXT_NFS_SERVER}:${EXT_NFS_BASE_EXPORT_PATH} ${TMP_MNT_PATH}
+sudo_mkdir_if_not_exist  "${TMP_MNT_PATH}/${NAME_PREFIX}"
+mkdir ${TMP_RESOURCE_DIR}
+cd ${TMP_MNT_PATH}/${NAME_PREFIX}
+for PV_FOLDER in ${PREDEFINED_PVS_TO_CREATE} 
+do
+	sudo_mkdir_if_not_exist "${PV_FOLDER}"
+done
+for PV_FOLDER in `seq -f "vol-%03g" 1 ${NUM_OF_PVS}`
+do
+	sudo_mkdir_if_not_exist  "${PV_FOLDER}"
+	cp ${PV_FILE_TEMPLATE} ${TMP_RESOURCE_DIR}/${PV_FOLDER}.yaml
+	sed -i "s/#VOL_NAME#/${PV_FOLDER}/g" ${TMP_RESOURCE_DIR}/${PV_FOLDER}.yaml
+	sed -i "s/#EXPORT_BASE#/${CLUSTER_EXT_NFS_BASE_EXPORT_PATH}/g" ${TMP_RESOURCE_DIR}/${PV_FOLDER}.yaml
+	sed -i "s/#NFS_SERVER#/${EXT_NFS_SERVER}/g" ${TMP_RESOURCE_DIR}/${PV_FOLDER}.yaml
+
+done
+sudo chmod 777 *
+cd ${BUILD_DIR}
+sudo umount ${TMP_MNT_PATH}
+
+
+
+
 
 #
 # Checkout openshift-ansible
@@ -66,6 +112,8 @@ export INSTALL_EXAMPLES
 export LDAP_PROVIDERS
 export MASTER_HOST_SPEC
 export REDHAT_IT_ROOT_CA_PATH
+export EXT_NFS_SERVER
+export EXT_NFS_BASE_EXPORT_PATH
 
 envsubst < ${INVENTORY_TEMPLATE_PATH} > ${INVENTORY_PATH}
 
@@ -75,6 +123,7 @@ cat ${INVENTORY_PATH}
 echo "#######################################################################"
 
 RETRCODE=0
+
 sshpass -p${ROOT_PASSWORD} \
                 ansible-playbook \
                   --user root \
