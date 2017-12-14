5da00a5575da34200033133fae16597b522e699a
Zohar Galor <zgalor@redhat.com>
2017-12-14 15:06:24 +0200

    Rename prometheus namespace to openshift-metrics in output plugin

--- plugins/osh_common_output.sh
+++ plugins/osh_common_output.sh
@@ -7,8 +7,8 @@ cat /etc/origin/master/ca.crt
 echo ""
 
 echo HAWKULAR_ROUTE=\"$(oc get route --namespace='openshift-infra' -o go-template --template='{{.spec.host}}' hawkular-metrics 2> /dev/null)\"
-echo PROMETHEUS_ALERTS_ROUTE=\"$(oc get route --namespace='prometheus' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)\"
-echo PROMETHEUS_METRICS_ROUTE=\"$(oc get route --namespace='prometheus' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)\"
+echo PROMETHEUS_ALERTS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)\"
+echo PROMETHEUS_METRICS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)\"
 echo OSH_HOST=\"$(oc get nodes -o name |grep master |sed -e 's/nodes\///g')\"
 echo OSH_TOKEN=\"$(oc sa get-token -n management-infra management-admin)\"
 echo OSH_CERT=\""$(cat /etc/origin/master/ca.crt)"\"
