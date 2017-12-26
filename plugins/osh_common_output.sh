48572f37a03900e8bf0679c8179281c4c63da3a4
Elad Alfassa <ealfassa@redhat.com>
2017-12-26 10:50:28 +0200

    Remove duplicate output from osh_common_output.sh

--- plugins/osh_common_output.sh
+++ plugins/osh_common_output.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo "management-admin token:"
-oc sa get-token -n management-infra management-admin
-echo ""
-
-echo "OpenShift Master ca crtificate"
-cat /etc/origin/master/ca.crt
-echo ""
-
 echo OPENSHIFT_HAWKULAR_ROUTE=\"$(oc get route --namespace='openshift-infra' -o go-template --template='{{.spec.host}}' hawkular-metrics 2> /dev/null)\"
 echo OPENSHIFT_PROMETHEUS_ALERTS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)\"
 echo OPENSHIFT_PROMETHEUS_METRICS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)\"
