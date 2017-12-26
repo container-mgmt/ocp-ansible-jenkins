5fd290439f88bcfad5e1aad44d6e7872574754e3
Elad Alfassa <ealfassa@redhat.com>
2017-12-26 10:49:06 +0200

    Rename common_output vars to be consistent with cm-ops-flow

--- plugins/osh_common_output.sh
+++ plugins/osh_common_output.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "management-admin token:"
 oc sa get-token -n management-infra management-admin
 echo ""
@@ -6,9 +7,9 @@ echo "OpenShift Master ca crtificate"
 cat /etc/origin/master/ca.crt
 echo ""
 
-echo HAWKULAR_ROUTE=\"$(oc get route --namespace='openshift-infra' -o go-template --template='{{.spec.host}}' hawkular-metrics 2> /dev/null)\"
-echo PROMETHEUS_ALERTS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)\"
-echo PROMETHEUS_METRICS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)\"
-echo OSH_HOST=\"$(oc get nodes -o name |grep master |sed -e 's/nodes\///g')\"
-echo OSH_TOKEN=\"$(oc sa get-token -n management-infra management-admin)\"
-echo OSH_CERT=\""$(cat /etc/origin/master/ca.crt)"\"
+echo OPENSHIFT_HAWKULAR_ROUTE=\"$(oc get route --namespace='openshift-infra' -o go-template --template='{{.spec.host}}' hawkular-metrics 2> /dev/null)\"
+echo OPENSHIFT_PROMETHEUS_ALERTS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)\"
+echo OPENSHIFT_PROMETHEUS_METRICS_ROUTE=\"$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)\"
+echo OPENSHIFT_MASTER_HOST=\"$(oc get nodes -o name |grep master |sed -e 's/nodes\///g')\"
+echo OPENSHIFT_MANAGEMENT_ADMIN_TOKEN=\"$(oc sa get-token -n management-infra management-admin)\"
+echo OPENSHIFT_CA_CRT=\""$(cat /etc/origin/master/ca.crt)"\"
