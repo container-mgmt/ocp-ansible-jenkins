45d192b8a7333e2d8e450a5f1d3a00bd0e504518
Elad Alfassa <ealfassa@redhat.com>
2018-01-29 10:32:23 +0200

    Enable various server roles when deploying ManageIQ (#28)
    Enable capacity&utilization (metrics), smart state analysis, and cockpit roles

--- miq_scripts/run.rb
+++ miq_scripts/run.rb
@@ -1,4 +1,13 @@
-print "Assinging alert profiles to the Enterprise\n"
+puts "Assinging alert profiles to the Enterprise"
 
 MiqAlertSet.find_by(:guid=>"a16fcf51-e2ae-492d-af37-19de881476ad").assign_to_objects(MiqEnterprise.last)
 MiqAlertSet.find_by(:guid=>"ff0fb114-be03-4685-bebb-b6ae8f13d7ad").assign_to_objects(MiqEnterprise.last)
+
+puts "Enabling C&U roles"
+
+server_config = MiqServer.my_server.get_config()
+roles = server_config.config[:server][:role].split(',')
+roles |= ['cockpit_ws', 'smartproxy', 'smartstate', 'ems_metrics_collector','ems_metrics_coordinator','ems_metrics_processor']
+roles = roles.sort().join(',')
+MiqServer.my_server.set_config(:server=>{:role => roles})
+MiqServer.my_server.save()
